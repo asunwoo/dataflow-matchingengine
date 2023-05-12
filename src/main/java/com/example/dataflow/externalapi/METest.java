9beaa4d69df0f9adee994d2738f9b99d8d3d3167
asunwoo24@gmail.com <asunwoo24@gmail.com>
2023-05-12 11:26:49 -0700

    Added auth to ME test

--- src/main/java/com/example/dataflow/externalapi/METest.java
+++ src/main/java/com/example/dataflow/externalapi/METest.java
@@ -3,11 +3,26 @@ package com.example.dataflow.externalapi;
 import com.google.cloud.aiplatform.v1beta1.MatchServiceClient;
 import com.google.cloud.aiplatform.v1beta1.FindNeighborsRequest;
 import com.google.cloud.aiplatform.v1beta1.FindNeighborsResponse;
+import com.google.cloud.aiplatform.v1beta1.MatchServiceSettings;
 import com.google.cloud.aiplatform.v1beta1.IndexEndpointName;
+
 import java.util.ArrayList;
+import java.io.FileInputStream;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.auth.Credentials;
+import com.google.api.gax.core.FixedCredentialsProvider;
 public class METest {
   public static void main(String[] args) {
-    try (MatchServiceClient matchServiceClient = MatchServiceClient.create()) {
+
+    try {
+      Credentials credentials = GoogleCredentials.fromStream(new FileInputStream("/home/asunwoo_mikewood_altostrat_com/vertex-samples-for-ck-0b622d103d10.json"));
+
+      MatchServiceSettings matchServiceSettings =
+          MatchServiceSettings.newBuilder().setCredentialsProvider(FixedCredentialsProvider.create(credentials)).build();
+
+      MatchServiceClient matchServiceClient = MatchServiceClient.create(matchServiceSettings);
+
       FindNeighborsRequest request =
           FindNeighborsRequest.newBuilder()
               .setIndexEndpoint(
